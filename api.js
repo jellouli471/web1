4661ad2edf5aafce0d50ba2f2612a3c36c714685
jellouli.dev <118634695+jellouli471@users.noreply.github.com>
2025-01-11 19:48:15 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_KEY = '2XaOU-JXqj93daZnjnI2p7Zk5U5xgVhIw945CWIQ_0g';
+const API_KEY = 'your api key';
 const API_BASE_URL = 'https://guided-talented-snapper.ngrok-free.app';
 
 // تصدير المتغيرات للاستخدام في الملفات الأخرى
