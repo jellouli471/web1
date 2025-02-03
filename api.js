e69895350915cf4568be92842cf3f89f4c65c433
jellouli.dev <118634695+jellouli471@users.noreply.github.com>
2025-02-03 19:22:37 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const API_KEY = 'your api key';
-const API_BASE_URL = 'https://guided-talented-snapper.ngrok-free.app';
+const API_BASE_URL = 'https://anime.apiclub.site';
 
 // تصدير المتغيرات للاستخدام في الملفات الأخرى
 export { API_KEY, API_BASE_URL };
